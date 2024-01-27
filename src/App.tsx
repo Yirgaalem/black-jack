641276c1470f8b88c1f0101355812257c1392492
Nahor Yirgaalem <nahor21@hotmail.com>
2024-01-26 19:47:41 -0500

    Adding a start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import Table from './Components/Table/Table';
 import './App.css';
+import { useState } from 'react';
 
 export default () => {
+  const initialPage: JSX.Element = (
+  <>
+    <div className='title'>BlackJack !</div>
+    <button onClick={()=> setPageToDisplay(<Table/>)}>Play</button>
+  </>
+  );
+  
+  const [pageToDisplay, setPageToDisplay] = useState(initialPage);
 
+  // setTimeout(() => {
+  //   setPageToDisplay(<Table/>);
+  // }, 2200);
+
+  console.log("h")
   return (
     <>
-      <Table/>
+      {pageToDisplay}
     </>
   );
 }
